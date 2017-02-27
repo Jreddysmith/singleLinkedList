aa8d5b6707079761321b390e01a905eb8513887a
Jarred Smith <jreddysmith@gmail.com>
2017-02-27 00:55:02 -0800

    making linked list

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -35,9 +35,41 @@ LinkedList.prototype.prepend = function(val) {
 
 };
 
+// 1.Create a new node using val
+// 2. Traverse to the end of the list
+// 3. Make the last node's 'next' value point to the new node
+LinkedList.prototype.append = function(val) {
+	var newNode = {
+		data: val,
+		next: null
+	};
+
+	if (this.isEmpty()) {
+		this.head = newNode;
+		return;
+	}
+
+	var current = this.head;
+
+	while (current.next !== null) {
+		current = current.next;
+
+	}
+
+	current.next = newNode;
+
+}
+
+LinkedList.prototype.remove = function(val) {
+	if (this.head.val === val)
+}
+
 var list = new LinkedList();
 list.prepend(10);
 list.prepend(5);
+list.append(2);
 
 console.log(list.size());
+console.log(list.isEmpty());
+console.log(list);
 
