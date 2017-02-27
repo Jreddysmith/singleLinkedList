5308e7b1dc5346bd9bef7768956363f505182280
Jarred Smith <jreddysmith@gmail.com>
2017-02-27 01:37:15 -0800

    finished linkedlist

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -58,18 +58,68 @@ LinkedList.prototype.append = function(val) {
 
 	current.next = newNode;
 
-}
+};
+
+LinkedList.prototype.contains = function(val) {
+	var current = this.head; 
+
+	while (current !== null) {
+		if (current.data === val) {
+			return true;
+		}
+		current = current.next;
+	}
+	return false;
+};
+
+// [5] -> [10] -> [15] -> [20] -> null
+// prev     current
 
 LinkedList.prototype.remove = function(val) {
-	if (this.head.val === val)
+	if (!this.contains(val)) {
+		return;
+	}
+
+	if (this.head.data === val) {
+		this.head = this.head.next;
+		return;
+	}
+
+	var prev = null;
+	var curr = this.head;
+
+	while (curr.data !== val) {
+		prev = curr;
+		curr = curr.next;
+	}
+
+	prev.next = curr.next;
+};
+
+LinkedList.prototype.print = function() {
+	var output = '[';
+	var current = this.head;
+
+	while (current !== null) {
+		output += current.data;
+		if(current.next !== null) {
+			output += ',';
+		}
+		current = current.next;
+	}
+
+	output += ']';
+	console.log(output);
 }
 
-var list = new LinkedList();
-list.prepend(10);
-list.prepend(5);
-list.append(2);
-
-console.log(list.size());
-console.log(list.isEmpty());
-console.log(list);
+// var list = new LinkedList();
+// list.append(10);
+// list.append(15);
+// list.append(20);
+// list.append(25);
+// list.prepend(5);
+// console.log(list.contains(7));
+// console.log(list.size());
+// console.log(list.isEmpty());
+// list.print();
 
